307c5ede8f7d44fe42edc3e0a52387da2e1115a5
takiguchi <takiguchi@arksystems.co.jp>
2020-07-15 14:05:27 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Backlog javascript action
+
+This action sync pull request to Backlog issues.
+
+## Inputs
+
+### `backlog-url`
+
+**Required** The URL of your backlog workspace.
+
+## Outputs
+
+## Example usage
+
+uses: actions/backlog-action@v1
+with:
+  url: 'https://workspace.backlog.com'
+
---
+++ src/main.ts
@@ -0,0 +1,46 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as Webhooks from "@octokit/webhooks";
+import axios from "axios";
+import querystring from "querystring";
+
+async function run() {
+  try {
+    const token = core.getInput("repo-token", { required: true });
+    const host = core.getInput("backlogHost", { required: true });
+    const apiKey = core.getInput("apk-key", { required: true });
+
+    if (github.context.eventName != "pull_request") {
+      core.error("Event should be a pull_request");
+    }
+    const pullRequestPayload = github.context
+      .payload as Webhooks.Webhooks.WebhookPayloadPullRequest;
+    const title = pullRequestPayload.pull_request.title;
+
+    for (const issueKey of parseIssueKey(title)) {
+      const apiUrl = `https://${host}/api/v2/issues/${issueKey}/comments?apiKey=${apiKey}`;
+
+      await axios.post(
+        apiUrl,
+        querystring.stringify({
+          content: `Pull request created: [#${pullRequestPayload.number}](${pullRequestPayload.pull_request.url})`,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+    }
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+function parseIssueKey(title: string): RegExpMatchArray {
+  let re = /([A-Z][A-Z0-9]+-\d+)/g;
+  return title.match(re) || [];
+}
+
+run();
